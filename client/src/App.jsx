4d92d4db40fc979a38a00b7d9fc672e4a5d0b458
Mehul Gupta <mehulguptar.w453@gmail.com>
2025-03-24 22:09:38 +0530

    Frontend Setup

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+function App() {
+
+  return (
+    <>
+  <div>
+    Hello
+  </div>
+    </>
+  )
+}
+
+export default App
